f6f7cce1cd5493b39ceeae7f4b09192d3abe90d4
Kristel Teng <kristelteng@gmail.com>
2014-10-21 14:39:28 -0400

    exercise2 complete

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+puts 55 * 0.15
+
+a = 10
+b = "My favorite number is\s"
+puts b + a.to_s 
+
+
+a = 45628 
+b = 7839
+puts "The value of a is #{a}." 
+puts "The value of b is #{b}."
+puts "The value of a multiply b is #{a * b}."
+
+puts true && false
+puts false && true
+puts !(false && false)
+
+
