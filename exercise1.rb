673ac1d57ed8eaa5ef101dc27e5045651604d53d
Kristel Teng <kristelteng@gmail.com>
2014-10-21 13:29:14 -0400

    Exercise 1 complete

--- exercise1.rb
+++ exercise1.rb
@@ -5,4 +5,25 @@ puts 2
 puts 3
 puts 2 != 3
 
-puts 2 puts 3 puts 2 != 3
+
+puts 'Hello world'
+puts "Hello world"
+
+puts "Betty's pie shop"
+puts 'Betty\'s pie shop'
+
+puts "Enter name"
+name = gets.chomp
+puts "Your name is #{name}"
+
+a = 20
+b = 65
+puts "The value of a is #{a}."
+puts "The value of a is #{b}."
+puts "The value of a plus b is #{a + b}." 
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+puts "Hello\sKristel"
