582723826630531f86c87080bd11bf5eb95d2918
Kristel Teng <kristelteng@gmail.com>
2014-10-21 15:33:55 -0400

    exercise3 complete

--- exercise3.rb
+++ exercise3.rb
@@ -3,3 +3,11 @@ puts "What is your name?"
 name = gets.chomp.strip
 puts "Hi #{name}!"
 
+puts "How old are you?"
+age = gets.chomp
+year_birth = 2014 - age.to_i
+
+puts "I am #{age} years old and I was born in #{year_birth}"
+
+
+
